528cbb8c75a70d5b23c5f9805edb0058ff2603d1
Kirill Pogorelov <23706384+kirillpogo-study@users.noreply.github.com>
2020-10-14 19:26:40 +0300

    feat: reverse int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  if (n.toString().split('')[0] === '-') {
+    return Number(n.toString().split('').reverse().join('').slice(0, -1))
+  }
+  return Number(n.toString().split('').reverse().join(''))
 }
